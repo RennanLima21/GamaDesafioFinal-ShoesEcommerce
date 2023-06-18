2b79e60495d643a4a9f4317fe329fd54ff6e34da
Rennan Lima <rennanlima@hotmail.com>
2023-06-18 19:18:55 -0400

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 /**
  * Alterado por Lívia Alcântara da Silva
- * 
+ *
  */
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./Router";
-import "./styles/global.css";
-import { Link } from "react-router-dom";
-import { useAppSelector } from "./hooks";
-import { RootState } from "./store";
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+import './styles/global.css';
+import Header from 'components/Header';
 
 function App() {
-  const estadoCarrinho = useAppSelector((state: RootState)=>state.carrinhoReducer);
-  const itensNoCarrinho = estadoCarrinho.carrinho.length;
   return (
     <BrowserRouter>
-    <div>
-    <div>
-      <header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/Produtos">Comprar</Link>
-          <input type="text" />
-        </nav>
-        <nav>
-          <Link to="/carrinho">Carrinho ({itensNoCarrinho})</Link>
-          <Link to="/Login">Login</Link>
-        </nav>
-      </header>
-    </div>
+      <Header />
       <Router />
-    </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+import { RootState } from 'store';
+import { useAppSelector } from 'hooks';
+import logo from "assets/images/logo.png"
+import './header.css'
+
+const Header = () => {
+  const estadoCarrinho = useAppSelector((state: RootState) => state.carrinhoReducer);
+  const itensNoCarrinho = estadoCarrinho.carrinho.length;
+  return (
+    <header className='container-header'>
+      <img src={logo} alt="logo" />
+      <nav className='container-nav'>
+        <Link to="/" className='link-header'>Home</Link>
+        <Link to="/Produtos" className='link-header'>Comprar</Link>
+        <Link to="/carrinho" className='link-header'>Carrinho ({itensNoCarrinho})</Link>
+        <Link to="/Login" className='link-header'>Login</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
