14f2252a33034e206449735b4332c74a336810ea
Rennan Lima <rennanlima@hotmail.com>
2023-06-18 23:13:49 -0400

    add footer app

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
 import './styles/global.css';
 import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Router />
+      <Footer />
     </BrowserRouter>
   );
 }
