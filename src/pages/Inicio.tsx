b89952aaf70fcbf061b871345867ca38ffcbbf06
Rennan Lima <rennanlima@hotmail.com>
2023-06-28 00:30:32 -0400

    page home + home.css

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,11 +1,28 @@
-/**Alterado por Lívia Alcântara da Silva
- * 
- */
+import logo from 'assets/images/logo1.png';
+import '../styles/inicio.css';
 
 export const Inicio = () => {
   return (
     <div className="footer">
-      <strong>Nossas novidades</strong>
-    </div>    
+      <section className="home-container">
+        <h1>
+          Bem-vindo ao <span>gama.Shoes</span>
+        </h1>
+        <p>Comece a pisar com estilo gama de ser.</p>
+        <a href="/">
+          <button className="btn-inicio">Ver Produtos gama</button>
+        </a>
+        <div className="carousel">
+          <div className="item">
+            <img src="" alt="Shoe" />
+          </div>
+          <div className="info">
+            <span className="name">Shuper Shoe1</span>
+            <span className="oldPrice">R$ 299,00</span>
+            <span className="price">R$ 199,00</span>
+          </div>
+        </div>
+      </section>
+    </div>
   );
-}
+};
